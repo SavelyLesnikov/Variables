797d741fb36db5d9460422c1963cbfb170c4538e
SavelyLesnikov <166781803+SavelyLesnikov@users.noreply.github.com>
2024-04-18 15:25:07 +0700

    Add files via upload

---
+++ Variables/Module1/homework5.py
@@ -0,0 +1,16 @@
+my_list = ['Яблоко', 'Банан', 'Апельсин', 'Груша', 'Мандарин']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:])
+my_list[2] = 'Манго'
+print(my_list)
+my_dict = {'Iron': 'Железо',
+           'Copper': 'Медь',
+           'Silver': 'Серебро',
+           'Gold': 'Золото'}
+print(my_dict)
+my_dict['Silver'] = '47/Ag'
+my_dict.update({'Magnesium': 'Магний',
+                'Lithium': 'Литий'})
+print(my_dict)
